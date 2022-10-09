9b2c07b1203b31b134e68d9ee3f488a8444a20d3
chenyw <xtchentingting@163.com>
2022-10-09 18:12:51 +0800

    demo01

---
+++ demo01/src/com/msb/Student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @program: git-demo
+ * @description: TODO
+ * @author: Miss.Chen
+ * @create 2022-10-09 17:33
+ **/
+public class Student {
+    private Integer id;
+    private String name;
+}
